185f4436e0768d6ba08e2d5d2255081015b3489f
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-09-01 18:33:53 -0700

    dev

--- scripts/merge-playwright-reports.js
+++ scripts/merge-playwright-reports.js
@@ -3,10 +3,9 @@ const { mergeHTMLReports } = require('playwright-merge-html-reports');
 console.log('Merging reports...');
 
 const reportDirName = '/playwright-report';
-const reportFileName = 'index.html';
 
 mergeHTMLReports([
-  process.cwd() + `${reportDirName}-1/${reportFileName}`,
-  process.cwd() + `${reportDirName}-2/${reportFileName}`,
-  process.cwd() + `${reportDirName}-3/${reportFileName}`,
+  process.cwd() + `${reportDirName}-1/`,
+  process.cwd() + `${reportDirName}-2/`,
+  process.cwd() + `${reportDirName}-3/`,
 ]).then(console.log('Reports merged!'));
